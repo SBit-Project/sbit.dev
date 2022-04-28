e7c9840a07816f1fb33e9ecb88222468b473e46e
behfarkhosravi <behfar.khosravi@gmail.com>
2022-04-28 14:04:17 +0430

    Update Features.tsx

--- src/partials/Features.tsx
+++ src/partials/Features.tsx
@@ -23,7 +23,7 @@ function Features() {
                                 <div className={"flex items-center mb-3"}>
                                     <div
                                         className="flex items-center justify-center text-white w-8 h-8 bg-gray-800 rounded-full shadow flex-shrink-0 mr-3">
-                                        {lightningIcon}
+                                        {tickIcon}
                                     </div>
                                     <h3 className="h3 m-0">
                                         EVM
@@ -78,7 +78,7 @@ function Features() {
 
                                     <div
                                         className="flex items-center justify-center text-white w-8 h-8 bg-gray-800 rounded-full shadow flex-shrink-0 ">
-                                        {arrowIcon}
+                                        {tickIcon}
                                     </div>
                                 </div>
                                 <p className="text-xl  dark:text-gray-200">
@@ -165,7 +165,7 @@ function Features() {
 
                                     <div
                                         className="flex items-center justify-center text-white w-8 h-8 bg-gray-800 rounded-full shadow flex-shrink-0 ">
-                                        {settingsIcon}
+                                        {tickIcon}
                                     </div>
                                 </div>
 
@@ -188,26 +188,6 @@ function Features() {
 }
 
 
-const lightningIcon = <svg
-    className="w-3 h-3 fill-current"
-    viewBox="0 0 12 12"
-    xmlns="http://www.w3.org/2000/svg"
->
-    <path
-        d="M11.953 4.29a.5.5 0 00-.454-.292H6.14L6.984.62A.5.5 0 006.12.173l-6 7a.5.5 0 00.379.825h5.359l-.844 3.38a.5.5 0 00.864.445l6-7a.5.5 0 00.075-.534z"/>
-</svg>;
-
-const arrowIcon = <svg
-    className="w-3 h-3 fill-current"
-    viewBox="0 0 12 12"
-    xmlns="http://www.w3.org/2000/svg"
->
-    <path
-        d="M11.854.146a.5.5 0 00-.525-.116l-11 4a.5.5 0 00-.015.934l4.8 1.921 1.921 4.8A.5.5 0 007.5 12h.008a.5.5 0 00.462-.329l4-11a.5.5 0 00-.116-.525z"
-        fillRule="nonzero"
-    />
-</svg>;
-
 const tickIcon = <svg width="16px" height="16px" viewBox="0 0 20 15"
                       version="1.1" xmlns="http://www.w3.org/2000/svg">
     <g id="Page-1" stroke="none" strokeWidth="1" fill="none"
@@ -220,13 +200,5 @@ const tickIcon = <svg width="16px" height="16px" viewBox="0 0 20 15"
     </g>
 </svg>;
 
-const settingsIcon = <svg xmlns="http://www.w3.org/2000/svg" height="24px"
-                          viewBox="0 0 24 24" width="16px" fill="#FFFFFF">
-    <g>
-        <path d="M0,0h24v24H0V0z" fill="none"/>
-        <path
-            d="M19.14,12.94c0.04-0.3,0.06-0.61,0.06-0.94c0-0.32-0.02-0.64-0.07-0.94l2.03-1.58c0.18-0.14,0.23-0.41,0.12-0.61 l-1.92-3.32c-0.12-0.22-0.37-0.29-0.59-0.22l-2.39,0.96c-0.5-0.38-1.03-0.7-1.62-0.94L14.4,2.81c-0.04-0.24-0.24-0.41-0.48-0.41 h-3.84c-0.24,0-0.43,0.17-0.47,0.41L9.25,5.35C8.66,5.59,8.12,5.92,7.63,6.29L5.24,5.33c-0.22-0.08-0.47,0-0.59,0.22L2.74,8.87 C2.62,9.08,2.66,9.34,2.86,9.48l2.03,1.58C4.84,11.36,4.8,11.69,4.8,12s0.02,0.64,0.07,0.94l-2.03,1.58 c-0.18,0.14-0.23,0.41-0.12,0.61l1.92,3.32c0.12,0.22,0.37,0.29,0.59,0.22l2.39-0.96c0.5,0.38,1.03,0.7,1.62,0.94l0.36,2.54 c0.05,0.24,0.24,0.41,0.48,0.41h3.84c0.24,0,0.44-0.17,0.47-0.41l0.36-2.54c0.59-0.24,1.13-0.56,1.62-0.94l2.39,0.96 c0.22,0.08,0.47,0,0.59-0.22l1.92-3.32c0.12-0.22,0.07-0.47-0.12-0.61L19.14,12.94z M12,15.6c-1.98,0-3.6-1.62-3.6-3.6 s1.62-3.6,3.6-3.6s3.6,1.62,3.6,3.6S13.98,15.6,12,15.6z"/>
-    </g>
-</svg>;
 
 export default Features;
