7a4b697586ba4d8038913c6f21e685627b837bcc
behfarkhosravi <behfar.khosravi@gmail.com>
2022-05-05 12:58:25 +0430

    Deploy website - based on fea6349b293e7770351f38a8c1c1ffb65162fdbb

--- assets/js/814f3328.8008baa9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksbit_website=self.webpackChunksbit_website||[]).push([[2535],{5641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"DDAO-Data-Management","permalink":"/sbit.dev/blog/ddad"},{"title":"Sbit Exchange Usage Guide and Info","permalink":"/sbit.dev/blog/exchange"},{"title":"PoS in Sbit","permalink":"/sbit.dev/blog/pos"}]}')}}]);
